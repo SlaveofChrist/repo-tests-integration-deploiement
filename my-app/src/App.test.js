3ed225c55f55c3c86c366e8771f9b51984239f88
Eliel WOTOBE <59283680+SlaveofChrist@users.noreply.github.com>
2025-02-14 10:47:17 +0100

    feat(creation): mise en place de test d'integration exemple

---
+++ my-app/src/App.test.js
@@ -0,0 +1,20 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('check counter on click me button', () => {
+  render(<App />);
+  const button = screen.getByRole('button');
+  const counter = screen.getByTestId('count')
+  expect(button).toBeInTheDocument();
+  expect(counter).toBeInTheDocument();
+  expect(counter).toHaveTextContent("0");
+  fireEvent.click(button);
+  expect(counter).toHaveTextContent("1");
+});
